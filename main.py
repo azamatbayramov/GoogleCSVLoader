5f7b261f7866ef6c4d0ac18b3322a626196999b8
Azamat Bayramov <bayramov.azamat04@gmail.com>
2022-07-26 13:17:58 +0500

    Refactor code in main.py

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ import csv
 
 
 def get_contacts(filename):
-    lst = []
+    contacts_list = []
     with open(filename, newline='', encoding='utf8') as csvfile:
         reader = csv.reader(csvfile)
 
-        dict_n = {j: i for i, j in enumerate(reader.__next__())}
+        index_dict = {j: i for i, j in enumerate(reader.__next__())}
 
         for row in reader:
-            lst.append(User(row, dict_n))
+            contacts_list.append(User(row, index_dict))
 
-    return lst
-
+    return contacts_list
